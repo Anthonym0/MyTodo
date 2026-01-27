101792299f363082d55f429ea4a68be7db195976
agranjon <agranjon@student.42.fr>
2026-01-27 14:41:26 +0100

    add cards, change background

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 
-
-
+const cards = {
+  1: { name: "test", todos: { name: "salut" } },
+  2: { name: "test", todos: { name: "salut" } },
+  3: { name: "test", todos: { name: "salut" } },
+  4: { name: "test", todos: { name: "salut" } },
+};
 
 
 
 
 document.addEventListener('DOMContentLoaded', (e) => {
-    
+    for (const id in cards) {
+        const card = cards[id];
+
+        const div = document.createElement('div');
+        div.classList = 'container ml-5';
+        div.id = 'card';
+        
+        document.appendChild(div);
+    }
 });
