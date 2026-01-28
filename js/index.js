cc52d32ea2dea53cb1352949f72449449a59cf0b
agranjon <agranjon@student.42.fr>
2026-01-28 06:50:54 +0100

    add cards

--- js/index.js
+++ js/index.js
@@ -1,22 +1,43 @@
 
 const cards = {
-  1: { name: "test", todos: { name: "salut" } },
-  2: { name: "test", todos: { name: "salut" } },
-  3: { name: "test", todos: { name: "salut" } },
-  4: { name: "test", todos: { name: "salut" } },
+  1: { title: "test", todos: { name: "salut" } },
+  2: { title: "test", todos: { name: "salut" } },
+  3: { title: "test", todos: { name: "salut" } },
+  4: { title: "test", todos: { name: "salut" } },
 };
 
 
-
+                // <div class="card">
+                //     <h1 class="ml-2" id="title">TITLE CARD</h1>
+                //     <div class="card-content">
+                //         <button class="button btn-card mt-3">TITLE TODO</button>
+                //         <button class="button btn-card mt-3">ADD NEW CARD</button>
+                //     </div>
+                // </div>
 
 document.addEventListener('DOMContentLoaded', (e) => {
     for (const id in cards) {
         const card = cards[id];
 
         const div = document.createElement('div');
-        div.classList = 'container ml-5';
-        div.id = 'card';
-        
-        document.appendChild(div);
+        div.classList = 'card';
+
+        const title = document.createElement('h1');
+        title.classList = 'ml-2';
+        title.id = 'title';
+        title.textContent = cards[id].title;
+        div.appendChild(title);
+
+        const card_content = document.createElement('div');
+        card_content.classList = 'card-content';
+        div.appendChild(card_content);
+
+        const btn_card = document.createElement('button');
+        btn_card.classList = 'button btn-card mt-3';
+        btn_card.textContent = cards[id].title;
+        card_content.appendChild(btn_card);
+
+        const wrapper = document.getElementById('wrapper');
+        wrapper.appendChild(div);
     }
 });
